349d547bbe8aa581b375038484d242f715bc3be7
harud3 <89560796+harud3@users.noreply.github.com>
2024-11-02 12:00:49 +0900

    Add files via upload

--- MEI.js
+++ MEI.js
@@ -1,26 +1,22 @@
-var img = document.getElementById('myImage');
+var fileInput = document.getElementById('fileInput');
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-img.onload = function() {
-  canvas.width = img.width;
-  canvas.height = img.height;
-  ctx.drawImage(img, 0, 0, img.width, img.height);
+fileInput.onchange = function(e) {
+  var file = e.target.files[0];
+  var reader = new FileReader();
+
+  reader.onload = function(e) {
+    var img = new Image();
+
+    img.onload = function() {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0, img.width, img.height);
+    };
+
+    img.src = e.target.result;
+  };
+
+  reader.readAsDataURL(file);
 };
-
-var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-var data = imageData.data;
-
-for (var i = 0; i < data.length; i += 4) {
-  var red = data[i];
-  var green = data[i + 1];
-  var blue = data[i + 2];
-
-  // 赤色を識別
-  if (red > 200 && green < 50 && blue < 50) {
-    // 透明にする
-    data[i + 3] = 0;
-  }
-}
-
-ctx.putImageData(imageData, 0, 0);
